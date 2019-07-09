557477d661dd4f07becf839a04d6c77fa80417d8
Maievskyi Vadym <30689925+VadymMai@users.noreply.github.com>
2019-07-10 01:11:56 +0300

    login, register, cart, checkout

---
+++ src/app/components/category-card/category-card.component.ts
@@ -0,0 +1,16 @@
+import { Input, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-card',
+  templateUrl: './category-card.component.html',
+  styleUrls: ['./category-card.component.css']
+})
+export class CategoryCardComponent implements OnInit {
+  @Input() category: object;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/checkout/checkout.component.spec.ts
+++ src/app/pages/checkout/checkout.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CatCardComponent } from './cat-card.component';
+import { CheckoutComponent } from './checkout.component';
 
-describe('CatCardComponent', () => {
-  let component: CatCardComponent;
-  let fixture: ComponentFixture<CatCardComponent>;
+describe('CheckoutComponent', () => {
+  let component: CheckoutComponent;
+  let fixture: ComponentFixture<CheckoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CatCardComponent ]
+      declarations: [ CheckoutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CatCardComponent);
+    fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
