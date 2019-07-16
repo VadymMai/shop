9d5f2364c128a95caf8b5c1b3532e9a601f9e0e6
vvvukraine@gmail.com <qazqaz11>
2019-07-16 18:02:10 +0300

    HTTP requests

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 
 @Component({
-    selector: 'app-category',
-    templateUrl: './category.component.html',
-    styleUrls: ['./category.component.css'],
-    providers: [DataService]
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+  providers: [DataService]
 })
 export class CategoryComponent implements OnInit {
 
-    products: object[] = [];
+  constructor(private dataService: DataService) {
+  }
 
-    constructor(private dataService: DataService) {
-    }
-
-    ngOnInit() {
-        this.products = this.dataService.getProducts();
-    }
+  ngOnInit() {
+    this.dataService.getProducts();
+  }
 
 }
